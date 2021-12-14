45b36f0a0dbc98fdfc985f4b95384bd3b2d349ef
Dmitriy <dimukai@yandex.ru>
2021-12-14 23:18:32 +0400

    Домашняя работа к первому уроку

---
+++ Home Work number 1/Home Work number 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Home_Work_number_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Привет введи свое имя!"); // Приветствие           
+            string name = Console.ReadLine(); //вводим имя
+            DateTime date = DateTime.Now;// дата
+            System.Console.WriteLine($"Привет {name}, сегодня {date}."); // выводим приветствие имя и дату на консоль 
+        }
+    }
+}
